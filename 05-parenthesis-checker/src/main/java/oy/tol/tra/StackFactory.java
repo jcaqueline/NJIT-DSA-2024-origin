d39e1f5efe0a76a54c24c4776be8681aa8cceb28
ShimanShe <curryssm@163.com>
2024-03-31 13:37:13 +0800

    ShimanShe

--- 05-parenthesis-checker/src/main/java/oy/tol/tra/StackFactory.java
+++ 05-parenthesis-checker/src/main/java/oy/tol/tra/StackFactory.java
@@ -3,7 +3,7 @@ package oy.tol.tra;
 /**
  * This class instantiates different types of stacks implementing the {@code StackInterface} interface.
  * <p>
- * TODO: Students, implement the createCharacterStack method for instantiating {@code StackImplementation<Character>}
+ 
  * objects in the this task.
  * 
  * @author Antti Juustila
@@ -14,11 +14,7 @@ public class StackFactory {
    private StackFactory() {
    }
 
-   /**
-    * Creates an instance of StackImplementation for Integer type.
-    * @return The stack object.
-    */
-    public static StackInterface<Integer> createIntegerStack() {
+   public static StackInterface<Integer> createIntegerStack() {
       return new StackImplementation<>();
    }
 
@@ -33,21 +29,20 @@ public class StackFactory {
 
    /**
     * Instantiates a stack of Characters using the stack default constructor.
-    * TODO: Students, implement this method in this task.
+  
     * @return The stack implementation holding Characters.
     */
    public static StackInterface<Character> createCharacterStack() {
-
-      return stack;
+       return new StackImplementation<>();
    }
 
    /**
     * Instantiates a stack of Characters using the stack default constructor.
-    * TODO: Students, implement this method in this task.
+   
     * @return The stack implementation holding Characters.
     */
     public static StackInterface<Character> createCharacterStack(int capacity) {
 
-      return stack;
+      return new StackImplementation<>(capacity);
    }
 }
---
+++ 05-parenthesis-checker/src/main/java/oy/tol/tra/StackImplementation.java
@@ -0,0 +1,132 @@
+package oy.tol.tra;
+
+public class StackImplementation<E> implements StackInterface<E> {
+
+    private Object [] itemArray;
+    private int capacity;
+    private int currentIndex = -1;
+    private static final int DEFAULT_STACK_SIZE = 10;
+    private int size;
+   
+ 
+ 
+ 
+    /**
+     * Allocates a stack with a default capacity.
+     * @throws StackAllocationException
+     */
+    public StackImplementation() throws StackAllocationException {
+       this(DEFAULT_STACK_SIZE);
+       
+    }
+ 
+    @SuppressWarnings("unchecked")
+    public StackImplementation(int capacity) throws StackAllocationException {
+       if (capacity < 2) {  
+          throw new StackAllocationException("Capacity must be at least 2");  
+      }  
+      
+      this.capacity = capacity; 
+      this.itemArray = (E[]) new Object[capacity];
+      this.size = 0;
+      this.currentIndex = -1;
+       
+    }
+    
+    @Override
+    public int capacity() {
+      
+       return  this.capacity; 
+       
+    }
+   
+    @SuppressWarnings({ "unchecked", "unused" })
+    private void resize()throws StackAllocationException {  
+       
+       capacity *= 2; 
+       E[] tempArray = (E[]) new Object[capacity]; 
+       if (tempArray == null) {  
+          throw new StackAllocationException("Failed to allocate memory for stack expansion");  
+      }   
+       System.arraycopy(itemArray, 0, tempArray, 0, size);  
+       itemArray = tempArray;    
+      
+   }  
+    @Override
+    public void push(E element) throws StackAllocationException, NullPointerException {
+       if (element == null) {  
+          throw new NullPointerException("Cannot push null element into the stack");  
+      }  
+      if (size == capacity) {  
+          resize();  
+      }  
+      itemArray[++currentIndex] = element;  
+      size++;  
+      
+                
+    }
+ 
+    @SuppressWarnings("unchecked")
+    @Override
+    public E pop() throws StackIsEmptyException {
+       if (currentIndex < 0) {  
+         throw new StackIsEmptyException("Expecting to get StackIsEmptyException when popping from empty stack.");  
+     }  
+     E item = (E) itemArray[currentIndex]; 
+     itemArray[currentIndex] = null;  
+     currentIndex--;                   
+     size--;                 
+     return item;  
+       
+    }
+ 
+    @SuppressWarnings("unchecked")
+    @Override
+    public E peek() throws StackIsEmptyException {
+       if (currentIndex < 0) {    
+          throw new StackIsEmptyException("Expecting to get StackIsEmptyException when peeking from empty stack");    
+      }  
+      return (E) itemArray[currentIndex];  
+    }
+ 
+    @Override
+    public int size() {
+      
+       return currentIndex+1;
+    }
+ 
+    
+    @Override
+    public void clear() {
+      
+       currentIndex = -1;
+       
+    }
+ 
+    @Override
+    public boolean isEmpty() {
+       if (currentIndex==-1){
+          return true;
+       }
+       return false;
+       
+       
+    }
+ 
+    @Override
+    public String toString() {
+       if (isEmpty()) {  
+          return "[]";  
+      }  
+       StringBuilder builder = new StringBuilder("[");
+       for (var index = 0; index <= currentIndex; index++) {
+          builder.append(itemArray[index].toString());
+          if (index < currentIndex) {
+             builder.append(", ");
+          }
+       }
+       builder.append("]");
+       return builder.toString();
+    }
+ }
+ 
