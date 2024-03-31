9bd91e95d4c527b2f5b898c9017d3888741623f4
ShimanShe <curryssm@163.com>
2024-03-31 13:11:08 +0800

    ShimanShe

--- 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
+++ 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
@@ -1,20 +1,28 @@
 package oy.tol.tra;  
   
-import java.util.LinkedList;  
+
   
 public class QueueImplementation<E> implements QueueInterface<E> {  
   
-    private LinkedList<E> queue;  
+    private Object [] itemArray; 
     private int initialCapacity;  
     private int currentSize;  
-    
+    private int head = 0;
+    private int tail = 0;
+
+    private static final int DEFAULT_QUEUE_SIZE = 10;
+    public QueueImplementation() throws QueueAllocationException{
+        this(DEFAULT_QUEUE_SIZE);
+    }
+
+
   
-    public QueueImplementation(int initialCapacity) {  
-        if (initialCapacity < 0) {  
-            throw new IllegalArgumentException("Initial capacity cannot be negative");  
-        }  
+    public QueueImplementation(int initialCapacity)throws QueueAllocationException {  
+        if (initialCapacity<2){
+            throw  new QueueAllocationException("Capacity must greater than 2");
+        }
+        itemArray = new Object[initialCapacity];  
         this.initialCapacity = initialCapacity;  
-        this.queue = new LinkedList<>();  
         this.currentSize = 0;  
     }  
   
@@ -22,61 +30,120 @@ public class QueueImplementation<E> implements QueueInterface<E> {
     public int capacity() {  
         return initialCapacity;  
     }  
-    private void expandCapacity() {  
-       
-        initialCapacity *= 2;  
-       
-        LinkedList<E> newQueue = new LinkedList<>(queue);  
-        queue = newQueue;  
-    }  
+   
   
     @Override  
     public void enqueue(E element) throws QueueAllocationException, NullPointerException {  
-        if (element == null) {  
-            throw new NullPointerException("Element cannot be null");  
-        }  
-        if (currentSize >= initialCapacity) {  
-            expandCapacity(); 
-        }  
-        queue.addLast(element);  
-        currentSize++;  
-    }  
+        if (element == null){
+            throw new NullPointerException("The element to enqueue can't be null");
+        }
+        
+        if (currentSize >= initialCapacity){
+            try {
+                int newCapacity = 2 * initialCapacity;
+                Object [] newArray = new Object[newCapacity];
+                int i = 0;
+               
+                while (i<currentSize){
+                    
+                    if (head+i<initialCapacity){
+                        newArray[i] = itemArray[head+i];
+                    }else {
+                   
+                        newArray[i] = itemArray[i-(initialCapacity-head)];
+                    }
+                    i++;
+                }
+                itemArray = newArray;
+                initialCapacity = newCapacity;
+                
+                head = 0;
+               
+                tail = currentSize;
+            } catch (OutOfMemoryError e) {
+                throw new QueueAllocationException("Failed to allocate more room for the stack.");
+            }
+        }
+       
+        itemArray[tail] = element;
+        if (tail == initialCapacity-1){
+            tail = 0;
+        }else {
+            tail = tail+1;
+        }
+        currentSize++;
+    }
+
+    
   
+    @SuppressWarnings("unchecked")
     @Override  
     public E dequeue() throws QueueIsEmptyException {  
         if (isEmpty()) {  
             throw new QueueIsEmptyException("Queue is empty");  
         }  
-        E item = queue.removeFirst(); 
-        currentSize--; 
-        return item;  
+        Object dequeueElement = itemArray[head];
+        itemArray[head] = null;
+        if (head == initialCapacity-1){
+            head = 0;
+        }else {
+            head = head+1;
+        }
+        currentSize--;
+        return (E) dequeueElement;
     }  
   
+    @SuppressWarnings("unchecked")
     @Override  
     public E element() throws QueueIsEmptyException {  
-        if (isEmpty()) {  
+        if (head == tail && currentSize != initialCapacity) {  
             throw new QueueIsEmptyException("Queue is empty");  
         }  
-        return queue.getFirst();  
+       
+        Object abb = itemArray[head];
+        return (E) abb;
     }  
   
     @Override  
     public int size() {  
-        return queue.size();  
+        return currentSize;  
     }  
   
     @Override  
     public boolean isEmpty() {  
-        return queue.isEmpty();  
+        if (head == tail && currentSize != initialCapacity){
+            return true;
+        }
+        else return false;
     }  
   
     @Override  
     public void clear() {  
-        queue.clear();  
+        for (int i=0;i <initialCapacity;i++){
+            itemArray[i] = null;
+        }
+        head = 0;
+        tail = 0;
+        currentSize = 0;
     }  
     @Override  
     public String toString() {  
        
-        return queue.toString();  
-    }  
+        StringBuilder builder = new StringBuilder("[");
+        int i = 0;
+        while (i<currentSize){
+            if (head+i<initialCapacity){
+                builder.append(itemArray[head+i].toString());
+            }else {
+                builder.append(itemArray[i-(initialCapacity-head)].toString());
+            }
+            if (i < currentSize-1) {
+                builder.append(", ");
+            }
+            i++;
+        }
+        builder.append("]");
+        return builder.toString();
+    }
 }
+
