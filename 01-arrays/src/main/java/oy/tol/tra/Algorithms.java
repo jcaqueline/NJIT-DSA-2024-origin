e35f865eb181e8b09002ce672293c3facd3d65de
ShimanShe <curryssm@163.com>
2024-04-03 17:05:44 +0800

    ShimanShe

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -4,33 +4,30 @@ package oy.tol.tra;
 public class Algorithms{
    
     
-    public static <T extends Comparable<T>> void sort(T [] array){
-        if(null != array && array.length > 1) {
-			int len = array.length;
-			boolean isSwaped = true;
-			T temp = null;
-			for(int i=0;i<len && isSwaped;i++) {
-				isSwaped = false;
-				for(int j=0;j<len-1-i;j++) {
-					if(array[j].compareTo(array[j+1]) > 0) {
-						temp = array[j];
-						array[j] = array[j+1];
-						array[j+1] = temp;
-						isSwaped = true;
-					}
-				}
-			}
-		}
-
-    }
     public static <T> void reverse(T [] array) {
         int i = 0;
-        while ( i< array.length/2) {
-            T temp = array[i];
-            array[i] = array[array.length-i-1];
-            array[array.length-i-1] = temp;
+        while (i < array.length/2) {
+            swap(array,i,array.length-i-1);
             i++;
         }
+    }
+
+
+    //The generic method for sorting an array: sort (bubble sort)
+    public static <T extends Comparable<T>> void sort(T [] array) {
+        for (int i=0;i<array.length-1;i++){
+            for (int j=0;j<array.length-1-i;j++){
+                if (array[j].compareTo(array[j+1]) > 0) {
+                    swap(array,j,j+1);
+                }
+
+            }
+        }
+    }
+	public static <T> void swap(T[] array,int i,int j){
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
 }
--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,59 +1,80 @@
 package oy.tol.tra;
 
-import java.util.Arrays;
+
 
 public class Algorithms {
-    public static int binarySearch(int key, Integer[] array, int low, int high) {  
-        if (high >= low) {  
-            int mid = low + (high - low) / 2;  
-  
-            // If the element is present at the middle itself  
-            if (array[mid] == key) {  
-                return mid;  
-            }  
-  
-            // If element is smaller than mid, then it can only be present in left subarray  
-            if (array[mid] > key) {  
-                return binarySearch(key, array, low, mid - 1);  
-            }  
-  
-            // Else the element can only be present in right subarray  
-            return binarySearch(key, array, mid + 1, high);  
-        }  
-  
-        // We reach here when element is not present in array  
-        return -1;  
-    }  
-    public static void fastSort(Integer[] array) {  
-        int n = array.length;  
-        for (int i = 0; i < n - 1; i++) {  
-            for (int j = 0; j < n - i - 1; j++) {  
-                if (array[j].compareTo(array[j + 1]) > 0) {  
-                    // Swap array[j+1] and array[j]  
-                    Integer temp = array[j];  
-                    array[j] = array[j + 1];  
-                    array[j + 1] = temp;  
-                }  
-            }  
-        }  
-    }  
-     public static void fastSort(String[] array) {  
-        Arrays.sort(array, String.CASE_INSENSITIVE_ORDER);   
-    }  
-    public static int binarySearch(String key, String[] array, int fromIndex, int toIndex) {  
-        return Arrays.binarySearch(array, fromIndex, toIndex, key);  
-    }  
-    public static <T> int linearSearch(T value, T[] array, int fromIndex, int toIndex) {  
-        for (int index = fromIndex; index < toIndex; index++) {  
-            if (value.equals(array[index])) {  
-                return index;  
-            }  
-        }  
-        return -1;  
-    }  
-  
+    public static <T> void reverse(T [] array) {
+        int i = 0;
+        while (i < array.length/2) {
+            swap(array,i,array.length-i-1);
+            i++;
+        }
+    }
+
+    
+    public static <T extends Comparable<T>> void sort(T [] array) {
+        for (int i=0;i<array.length-1;i++){
+            for (int j=0;j<array.length-1-i;j++){
+                if (array[j].compareTo(array[j+1]) > 0) {
+                    swap(array,j,j+1);
+                }
+
+            }
+        }
+    }
+
+    public static <E extends Comparable<E>> void fastSort(E [] array) {
+        if (array == null || array.length <= 1){
+            return;
+        }
+        quickSort(array,0,array.length-1);
+    }
+
+    
+    public static <E extends Comparable<E>> void quickSort(E [] array, int begin, int end){
+        if (begin < end){
+            int q = partition(array,begin,end);
+            quickSort(array,begin,q-1);
+            quickSort(array,q+1,end);
+        }
+    }
+
+    
+    private static <E extends Comparable<E>> int partition(E [] array, int begin, int end) {
+        int i = begin-1;
+        for (int leftIndex = begin;leftIndex<end;leftIndex++){
+            if (array[leftIndex].compareTo(array[end])<0){
+                i++;
+                swap(array,i,leftIndex);
+            }
+        }
+        swap(array,i+1,end);
+        return i+1;
+    }
+
    
-    public static <T> int linearSearch(T value, T[] array) {  
-        return linearSearch(value, array, 0, array.length);  
-    }  
+    public static <T> void swap(T[] array,int i,int j){
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static <T extends Comparable<T>> int binarySearch(T aValue, T [] fromArray, int fromIndex, int toIndex) {
+        if (fromIndex > toIndex) {
+            return -1;
+        }
+
+        int middle = (fromIndex+toIndex)/2;
+
+        if (aValue.compareTo(fromArray[middle]) == 0) {
+            return middle;
+        }
+        else if (aValue.compareTo(fromArray[middle]) < 0) {
+            return binarySearch(aValue, fromArray, fromIndex, middle - 1);
+        }
+        else {
+            return binarySearch(aValue, fromArray, middle + 1, toIndex);
+        }
+
+    }
 }
